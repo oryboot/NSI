8c3bb7e27a2c5c1a0876f10e037f5628b66ded58
oxXLucasXxo <lucas.tolan@protonmail.com>
2024-11-25 23:30:55 +0100

    finish init for each subclasses

--- Jeu console/dependencies/menu.py
+++ Jeu console/dependencies/menu.py
@@ -1,4 +1,4 @@
-from .bird import Red, Chuck, Bomb
+from .bird import Red, Chuck, Bomb, birds_data
 
 bird_names = ['chuck', 'red', 'bomb']
 
@@ -17,11 +17,11 @@ def valid_name(name):
 
 def assign_bird(name):
     if name == 'red':
-        player = Red()
+        player = Red(**birds_data['Red'])
     elif name == 'chuck':
-        player = Chuck()
+        player = Chuck(**birds_data['Chuck'])
     elif name == 'bomb':
-        player = Bomb()
+        player = Bomb(**birds_data['Bomb'])
     return player
 
 def create_player():
@@ -37,8 +37,7 @@ def menu(start_game):
         print("Bienvenue dans Oiseaux en Colère !\nDans ce jeu votre objectif sera d'éliminer la menace Picoo.\n\n   1-Débuter le jeu  2-Règles")
         choice = input()
         if choice == '2':
-            print(
-                "Voici les règles du jeu :\nVous incarnez un oiseau au choix parmi Red, Chuck et Bomb. Chacun a ses statistiques propres.\nVotre but est de récupérer un oeuf que les méchants cochons, les Picoos, vous ont volé. Déterminé comme jamais,\nvous devrez passer à travers différents stages et affronter différents Picoo pour récupérer votre précieux oeuf auprès du Maléfique King Picoo.")
+            print("Voici les règles du jeu :\nVous incarnez un oiseau au choix parmi Red, Chuck et Bomb. Chacun a ses statistiques propres.\nVotre but est de récupérer un oeuf que les méchants cochons, les Picoos, vous ont volé. Déterminé comme jamais,\nvous devrez passer à travers différents stages et affronter différents Picoo pour récupérer votre précieux oeuf auprès du Maléfique King Picoo.")
         elif choice == '1':
             start_game = True
     return start_game
