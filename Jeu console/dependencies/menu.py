68372345d3784f977d1b6866cb54d42d928a38c3
lucas.tolan <lucas.tolan@monlycee.net>
2024-11-25 22:09:13 +0100

    fix menu issues and improve readability

--- Jeu console/dependencies/menu.py
+++ Jeu console/dependencies/menu.py
@@ -19,23 +19,21 @@ def assign_player_bird(name):
     player = Bird(name)
     return player
 
-def create_player(start):
-    if start:
+def create_player():
+    nom = choose_character()  # on demande au joueur de choisir un oiseau
+    while not valid_name(nom):
+        print('Valeur invalide !')
         nom = choose_character()  # on demande au joueur de choisir un oiseau
-        while not valid_name(nom):
-            nom = choose_character()  # on demande au joueur de choisir un oiseau
-            if valid_name(nom):
-                print('Valeur invalide !')
-        return assign_player_bird(nom)
+    return assign_player_bird(nom)
 
 
-def menu(play):
-    while not play:
+def menu(start_game):
+    while not start_game:
         print("Bienvenue dans Oiseaux en Colère !\nDans ce jeu votre objectif sera d'éliminer la menace Picoo.\n\n   1-Débuter le jeu  2-Règles")
         choice = input()
         if choice == '2':
             print(
                 "Voici les règles du jeu :\nVous incarnez un oiseau au choix parmi Red, Chuck et Bomb. Chacun a ses statistiques propres.\nVotre but est de récupérer un oeuf que les méchants cochons, les Picoos, vous ont volé. Déterminé comme jamais,\nvous devrez passer à travers différents stages et affronter différents Picoo pour récupérer votre précieux oeuf auprès du Maléfique King Picoo.")
         elif choice == '1':
-            play = True
-    return play
+            start_game = True
+    return start_game
